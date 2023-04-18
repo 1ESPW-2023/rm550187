19614e6ec978a5f6edd6b35eb45c47cb1b4d4bb8
FreitasRj1 <rm550187@fiap.com.br>
2023-04-18 20:59:33 -0300

    css com js e html

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
-//Exemplo de funções
-const divElement = document.getElementById("div-box")
+// //Exemplo de funções
+// const divElement = document.getElementById("div-box")
 
-//divElement.addEventListener("click" ,()=>{
-  //  console.log(divElement);
-//});
-divElement.addEventListener("click",mostraElemento);
+// //divElement.addEventListener("click" ,()=>{
+//   //  console.log(divElement);
+// //});
+// divElement.addEventListener("click",mostraElemento);
 
-function mostraElemento() {
-    console.log("divElement")
-}
+// function mostraElemento() {
+//     console.log("divElement")
+// }
 
-function soma(){
-    let n1 = 10
-    let n2 = 5
-    console.log(n1 + n2)
-}
+// function soma(){
+//     let n1 = 10
+//     let n2 = 5
+//     console.log(n1 + n2)
+// }
+
+
+
+const arrayElements = [...document.querySelectorAll("li")];
+
+arrayElements.map ((el,key)=>{
+
+
+  if(el.textContent  == indexof("1") != (-1)){
+      el.textContent  =`[${el.textContext}]`;
+  }
+});
+
