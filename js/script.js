e3f7a43dae0ffa207b6d2fd96e0b2d0d0d08663b
FreitasRj1 <rm550187@fiap.com.br>
2023-04-18 21:01:38 -0300

    ultima att aula

--- js/script.js
+++ js/script.js
@@ -18,13 +18,33 @@
 
 
 
-const arrayElements = [...document.querySelectorAll("li")];
+// const arrayElements = [...document.querySelectorAll("li")];
 
-arrayElements.map ((el,key)=>{
+// arrayElements.map ((el,key)=>{
 
 
-  if(el.textContent  == indexof("1") != (-1)){
-      el.textContent  =`[${el.textContext}]`;
-  }
-});
+//   if(el.textContent  == indexof("1") != (-1)){
+//       el.textContent  =`[${el.textContext}]`;
+//   }
+// });
+let tmp = 0
+function mudaCor(){
+  let r = Math.ceil(Math.random()*255);
+  let g = Math.ceil(Math.random()*255);
+  let b = Math.ceil(Math.random()*255);
+
+   const elemento = [...document.getElementsByClassName("conteudo")];
+
+   elemento.forEach((el)=>{
+    el.backgroundColor =`rgb(${r},rgb(${g},rgb(${b},`
+   })
+
+  tmp = setTimeout(mudaCor , 5000)
+}
+
+function paraTimeout(){
+    clearTimeout(tmp)
+}
+
+mudaCor();
 
